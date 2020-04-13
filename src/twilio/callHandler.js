53c379e8d4d02d04642f2f9f1317a795931081c1
Michael Maurer <mjmaurer777@gmail.com>
2020-04-13 04:00:38 -0400

    fix: remove loop

--- src/twilio/callHandler.js
+++ src/twilio/callHandler.js
@@ -1,17 +1,11 @@
 const twilio = require("twilio");
 
 module.exports = (req, res) => {
-  console.log(req.body);
   const response = new twilio.twiml.VoiceResponse();
-  response.play(
-    {
-      loop: 3
-    },
-    "https://bazaar-impala-3802.twil.io/assets/voicemail.mp3"
-  );
+  response.play("https://bazaar-impala-3802.twil.io/assets/voicemail.mp3");
 
   response.record({
-    action: "/call-handler-callback"
+    action: "/twilio/call-handler-callback"
   });
 
   res.set("Content-Type", "text/xml");
