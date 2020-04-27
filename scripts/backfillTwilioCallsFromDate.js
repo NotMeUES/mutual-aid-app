d4eecac6fe5bcf23491aa730e18bb8c941553127
Michael Maurer <mjmaurer777@gmail.com>
2020-04-27 14:32:12 -0400

    feat: script for backfilling missed twilio calls

--- scripts/backfillTwilioCallsFromDate.js
+++ scripts/backfillTwilioCallsFromDate.js
@@ -1,25 +1,69 @@
 const csv = require("csv-parser");
 const fs = require("fs");
+const { join } = require("path");
+const {
+  createRequest,
+  findRequestByPhone
+} = require("~airtable/tables/requests");
 
-const results = [];
-const client = require("twilio")("", authToken);
+let results = 0;
 
-fs.createReadStream("recordings.csv")
-  .pipe(csv())
-  .on("data", data => {
-    const rid = data.Sid;
+const numberMapping = {};
 
-    results.push(data);
-  })
-  .on("end", () => {
-    console.log(results);
-    client
-      .recordings("REXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-      .fetch()
-      .then(recording => console.log(recording.callSid));
+const sinceDate = new Date("18:40:00 UTC 2020-04-21");
 
-    // [
-    //   { NAME: 'Daffy Duck', AGE: '24' },
-    //   { NAME: 'Bugs Bunny', AGE: '22' }
-    // ]
-  });
+(async () => {
+  // calls.csv and recordings.csv can be downloaded from Twilio
+  fs.createReadStream(join(__dirname, "./calls.csv"))
+    .pipe(csv())
+    .on("data", data => {
+      if (new Date(data.StartTime).getTime() > sinceDate.getTime()) {
+        numberMapping[data.Sid] = data.From;
+      }
+    })
+    .on("end", () => {
+      fs.createReadStream(join(__dirname, "./recordings.csv"))
+        .pipe(
+          csv({
+            mapHeaders: ({ header }) => {
+              if (header.includes("RSid")) {
+                return "Sid";
+              }
+              return header;
+            }
+          })
+        )
+        .on("data", async data => {
+          if (numberMapping[data.CallSid]) {
+            const twilioSid = data.CallSid;
+            const recordingUrl = `https://api.twilio.com/2010-04-01/Accounts/${process.env.TWILIO_ACCOUNT_ID}/Recordings/${data.Sid}`;
+            const phone = numberMapping[data.CallSid];
+            let status = "";
+
+            results += 1;
+            const [request, _err] = await findRequestByPhone(phone);
+            if (request) {
+              status = "Duplicate";
+            }
+
+            const newRequest = {
+              message: recordingUrl,
+              twilioSid,
+              phone,
+              status,
+              source: "voice"
+            };
+            const [record, e] = await createRequest(newRequest);
+            if (record) {
+              console.log(`New record from twilio voice: ${record.getId()}`);
+            }
+            if (e) {
+              console.log(`Error: ${e}`);
+            }
+          }
+        })
+        .on("end", () => {
+          console.log(results);
+        });
+    });
+})();
